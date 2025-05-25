549db786970d40653ba68ef0a1a0fedadcc75835
Sercan Leylek <sercan.leylek@fremtind.no>
2025-05-25 22:44:31 +0200

    Search text without useEffect

--- src/components/LatestEarthquakes/LatestEarthquakes.tsx
+++ src/components/LatestEarthquakes/LatestEarthquakes.tsx
@@ -2,11 +2,10 @@ import './LatestEarthquakes.scss';
 import { IEarthQuakeLog, ILatestEarthquakes } from "../../utilities/interfaces.ts";
 import { TextInput } from '@fremtind/jokul/components/text-input';
 import LogRow from "../LogRow/LogRow.tsx";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const LatestEarthquakes: React.FC<ILatestEarthquakes> = ({ allEarthquakes }) => {
     const [searchText, setSearchText] = useState<string>('');
-    const [eqLogs, setEqLogs] = useState<IEarthQuakeLog[]>(allEarthquakes);
 
     const LIMIT = 20;
 
@@ -14,18 +13,16 @@ const LatestEarthquakes: React.FC<ILatestEarthquakes> = ({ allEarthquakes }) => 
         setSearchText(e.target.value);
     };
 
-    useEffect(() => {
-        if (searchText.trim() === '') {
-            setEqLogs(allEarthquakes.slice(0, LIMIT));
-        } else {
-            const someLogs = allEarthquakes.filter((log) => {
-                const location = log.Location.toLowerCase();
-                const searchTextLower = searchText.toLowerCase();
-                return location.indexOf(searchTextLower) > -1;
-            });
-            setEqLogs(someLogs);
-        }
-    }, [searchText]);
+    let eqLogs:IEarthQuakeLog[] = [];
+    if (searchText.trim() === '') {
+        eqLogs = allEarthquakes.slice(0, LIMIT);
+    } else {
+        eqLogs = allEarthquakes.filter((log) => {
+            const location = log.Location.toLowerCase();
+            const searchTextLowerCase = searchText.toLowerCase();
+            return location.indexOf(searchTextLowerCase) > -1;
+        });
+    }
 
     return (
       <div className="latestEarthquakes">
