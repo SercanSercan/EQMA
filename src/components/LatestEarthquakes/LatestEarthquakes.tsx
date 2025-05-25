e2e55adb3d8969c79b656b2516f88b8665bce82e
Sercan Leylek <sercan.leylek@fremtind.no>
2025-05-25 22:19:41 +0200

    Add search box

--- src/components/LatestEarthquakes/LatestEarthquakes.tsx
+++ src/components/LatestEarthquakes/LatestEarthquakes.tsx
@@ -1,13 +1,30 @@
 import './LatestEarthquakes.scss';
 import { IEarthQuakeLog, ILatestEarthquakes } from "../../utilities/interfaces.ts";
+import { TextInput } from '@fremtind/jokul/components/text-input';
 import LogRow from "../LogRow/LogRow.tsx";
+import { ChangeEvent, useState } from "react";
 
 const LatestEarthquakes: React.FC<ILatestEarthquakes> = ({ allEarthquakes }) => {
+    const [searchText, setSearchText] = useState<string>('');
+
     const LIMIT = 20;
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    }
 
     return (
       <div className="latestEarthquakes">
           <h2>Latest Earthquakes</h2>
+          <TextInput
+              label={''}
+              onChange={handleChange}
+              value={searchText}
+              maxLength={30}
+              placeholder={'Search location'}
+              autoComplete={'off'}
+              className={'latestEarthquakes__searchBox'}
+          />
           <div className="latestEarthquakes__list">
               {allEarthquakes.slice(0, LIMIT).map((earthquake: IEarthQuakeLog, idx) => (
                   <LogRow earthquakeLog={earthquake} key={idx} />
